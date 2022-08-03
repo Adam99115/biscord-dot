1f0400f736e70b0c75522ad3b75dec28cdd84ad7
Adam99115 <76864895+Adam99115@users.noreply.github.com>
2022-08-03 13:03:57 +1000

    Added auto stock checker based on portfolio listed in .env as JSON List. (#1)

--- biscord-dot.py
+++ biscord-dot.py
@@ -1,21 +1,47 @@
 import os
 import discord
+import time
+from datetime import datetime
+import json
 from dotenv import load_dotenv
-from discord.ext import tasks
+from discord.ext import commands, tasks
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 guild = os.getenv('DISCORD_GUILD')
-client = discord.Client()
+ownerId = os.getenv('OWNER_ID')
+bot = commands.Bot(command_prefix='`')
 
-@client.event
-async def on_read():
-        print("I've arrived")
-        
-@client.event
-async def on_message(message):
-       print("I've received", message)
-       StonksChecker = Daemon()
+@bot.event
+async def on_ready():
+    StonksChecker = Daemon()
+
+def getStockValue(exchange: str, stock: str):
+    browser.get('https://www.google.com/finance/quote/' + stock + ':' + exchange)
+    stockPrice = browser.find_element(By.CLASS_NAME, 'kf1m0').text
+    dayPercentage = browser.find_element(By.CLASS_NAME, 'JwB6zf').get_property('textContent')
+    print(dayPercentage)
+    color = browser.find_element(By.CLASS_NAME, 'VOXKNe').value_of_css_property('color')
+    if (color == 'rgba(165, 14, 14, 1)'):
+        description = " down " + dayPercentage
+    else: 
+        description = " up " + dayPercentage
+    return ('-' * 4 + exchange + ':' + stock + ' is ' + stockPrice + description)
+
+@bot.command()
+async def stonk(ctx, *args):
+    exchange = args[0]
+    stock = args[1]
+    await ctx.author.send(getStockValue(exchange=exchange, stock=stock))
 
 class Daemon():
     ##Background checker that can routinely check my stocks for me
@@ -23,11 +49,18 @@ class Daemon():
         self.interval = 0
         self.daemon_task.start()
 
-    @tasks.loop(seconds=3)
+    @tasks.loop(minutes=30)
     async def daemon_task(self):
-        print(self.interval)
-        self.interval += 1
+        stocks = json.loads(os.getenv('STOCKS'))
+        owner = await bot.fetch_user(ownerId)
+        await owner.send("Stocks update at " + datetime.now().strftime("%H:%M:%S"))
+        for stonk in stocks:
+            code, exchange = stonk.split(':')
+            time.sleep(1)
+            value = getStockValue(exchange,code)
+            await owner.send(value)
         
-client.run(token)
-
-#TODO : Add command handling, selenium for stocks / news updates, MOTDS, music / spotify playlist player ?
+bot.run(token)
+#TODO: Add command handling, 
+#TODO: news updates, MOTDS, music / spotify playlist player ?
+#TODO: get personalIp for owner.
